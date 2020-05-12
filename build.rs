bf0f3d9253cbe10eadab598a7d2f6139c862d277
xiao.huang@enmotech.com <xiao.huang@enmotech.com>
2020-05-12 11:20:22 +0800

    chore: add link

--- build.rs
+++ build.rs
@@ -1,3 +1,30 @@
+extern crate pkg_config;
+
+#[cfg(target_env = "msvc")]
+extern crate vcpkg;
+
 fn main() {
-    println!("cargo:rustc-flags=-l dylib=ssh")
+    if try_vcpkg() {
+        return;
+    }
+
+    pkg_config::Config::new()
+        .atleast_version("0.8")
+        .statik(false)
+        .probe("libssh")
+        .expect("dynamically linked libssh >= 0.8 is required");
+}
+
+#[cfg(not(target_env = "msvc"))]
+fn try_vcpkg() -> bool {
+    false
+}
+
+#[cfg(target_env = "msvc")]
+fn try_vcpkg() -> bool {
+    vcpkg::Config::new()
+        .atleast_version("0.8")
+        .statik(false)
+        .probe("libssh")
+        .expect("dynamically linked libssh >= 0.8 is required");
 }
