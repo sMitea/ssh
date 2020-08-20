a3c3e1168a722cf16edf01b46119d8c74dc68801
ShawHrown <shawhrown@163.com>
2020-08-20 22:23:14 +0800

    feat: update build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate pkg_config;
 #[cfg(target_env = "msvc")]
 extern crate vcpkg;
 
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::{env, process::Command};
 
 fn check_update_git() {
@@ -12,6 +12,12 @@ fn check_update_git() {
             .status();
     }
 }
+
+#[cfg(target_os = "macos")]
+const INSTALL_PATH : &str = "/usr/local";
+
+#[cfg(target_os = "linux")]
+const INSTALL_PATH : &str = "/usr/local";
 
 fn main() {
     println!("cargo:rerun-if-env-changed=LIBSSH_SYS_USE_PKG_CONFIG");
@@ -25,14 +31,11 @@ fn main() {
     }
 
     check_update_git();
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let mut cfg = cmake::Config::new("libssh");
     cfg.define("WITH_EXAMPLES", "OFF");
     cfg.define("UNIT_TESTING", "OFF");
     cfg.define("BUILD_STATIC_LIB", "ON");
     cfg.define("BUILD_SHARED_LIBS", "OFF");
-    cfg.define("CMAKE_INSTALL_PREFIX", format!("{}", out_dir.display()));
-    let dst = cfg.build();
-    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
-    println!("cargo:rustc-link-lib=static={}", "ssh");
+    cfg.define("CMAKE_INSTALL_PREFIX", INSTALL_PATH);
+    let _ = cfg.build();
 }
